9782ab23ac958b88cb0af97bd4af70a6b515c6de
CryptJS13 <jasper@slimmens.nl>
2023-12-19 22:27:30 +0100

    Update tests

--- test/core/vault_upgrade.js
+++ test/core/vault_upgrade.js
@@ -12,23 +12,20 @@ const BigNumber = require("bignumber.js");
 const IERC20 = artifacts.require("IERC20");
 
 //const Strategy = artifacts.require("");
-const Strategy = artifacts.require("CaviarStrategyMainnet_CVR");
+const Strategy = artifacts.require("AuraStrategyMainnet_jEUR_PAR");
+const VaultProxyContract = artifacts.require("VaultProxy");
 
-// Developed and tested at blockNumber 47097600
+// Developed and tested at blockNumber 46661610
 
 // Vanilla Mocha test. Increased compatibility with tools that integrate Mocha.
-describe("Arbitrum Mainnet Caviar CVR", function() {
+describe("Polygon Mainnet Balancer 2EUR PAR", function() {
   let accounts;
 
   // external contracts
   let underlying;
 
   // external setup
-  let underlyingWhale = "0xbDC1851f669f1E2Fef5990e86C2Fb2CbCC8552B6";
-  let cvr = "0x6AE96Cc93331c19148541D4D2f31363684917092";
-  let usdr = "0x40379a439D4F6795B6fc9aa5687dB461677A2dBa";
-  let pearl = "0x7238390d5f6F64e67c3211C343A410E2A3DEc142";
-  let weth = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+  let underlyingWhale = "0xD927cE147f098cE634301e6c4281541b1939a132";
 
   // parties in the protocol
   let governance;
@@ -43,13 +40,13 @@ describe("Arbitrum Mainnet Caviar CVR", function() {
   let strategy;
 
   async function setupExternalContracts() {
-    underlying = await IERC20.at("0x6AE96Cc93331c19148541D4D2f31363684917092");
+    underlying = await IERC20.at("0x513CdEE00251F39DE280d9E5f771A6eaFebCc88E");
     console.log("Fetching Underlying at: ", underlying.address);
   }
 
   async function setupBalance(){
     let etherGiver = accounts[9];
-    await web3.eth.sendTransaction({ from: etherGiver, to: underlyingWhale, value: 10e18});
+    await web3.eth.sendTransaction({ from: etherGiver, to: underlyingWhale, value: 100e18});
 
     farmerBalance = await underlying.balanceOf(underlyingWhale);
     await underlying.transfer(farmer1, farmerBalance, { from: underlyingWhale });
@@ -65,23 +62,22 @@ describe("Arbitrum Mainnet Caviar CVR", function() {
     await impersonates([governance, underlyingWhale]);
 
     let etherGiver = accounts[9];
-    await web3.eth.sendTransaction({ from: etherGiver, to: governance, value: 10e18});
+    await web3.eth.sendTransaction({ from: etherGiver, to: governance, value: 100e18});
 
     await setupExternalContracts();
     [controller, vault, strategy] = await setupCoreProtocol({
-      "existingVaultAddress": "0xCB2f2895208c36F38c9B13aB0C9e49Ad69B14e9d",
+      "existingVaultAddress": "0x41f36BB5B26F2E8646E6ADC97FAA5E2844a6c842",
+      "announceStrategy": true,
       "strategyArtifact": Strategy,
       "strategyArtifactIsUpgradable": true,
-      "announceStrategy": true,
       "underlying": underlying,
       "governance": governance,
-      "liquidation": [
-        {"pearl": [cvr, pearl, usdr]},
-        {"pearl": [usdr, pearl, cvr]},
-        {"pearl": [usdr, weth]},
-      ]
     });
 
+    await vault.scheduleUpgrade("0x7424a4D57F8c223d58EC19348780EAa237F6b286", {from: governance});
+    await Utils.waitHours(13);
+    const vaultProxy = await VaultProxyContract.at(vault.address);
+    await vaultProxy.upgrade({from: governance});
     // whale send underlying to farmers
     await setupBalance();
   });
@@ -91,11 +87,11 @@ describe("Arbitrum Mainnet Caviar CVR", function() {
       let farmerOldBalance = new BigNumber(await underlying.balanceOf(farmer1));
       await depositVault(farmer1, underlying, vault, farmerBalance);
 
+      // Using half days is to simulate how we doHardwork in the real world
       let hours = 10;
-      let blocksPerHour = 3600;
+      let blocksPerHour = 1565*5;
       let oldSharePrice;
       let newSharePrice;
-
       for (let i = 0; i < hours; i++) {
         console.log("loop ", i);
 
@@ -107,8 +103,8 @@ describe("Arbitrum Mainnet Caviar CVR", function() {
         console.log("new shareprice: ", newSharePrice.toFixed());
         console.log("growth: ", newSharePrice.toFixed() / oldSharePrice.toFixed());
 
-        apr = (newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/1800))*365;
-        apy = ((newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/1800))+1)**365;
+        apr = (newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/1565))*365;
+        apy = ((newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/1565))+1)**365;
 
         console.log("instant APR:", apr*100, "%");
         console.log("instant APY:", (apy-1)*100, "%");
@@ -119,8 +115,8 @@ describe("Arbitrum Mainnet Caviar CVR", function() {
       let farmerNewBalance = new BigNumber(await underlying.balanceOf(farmer1));
       Utils.assertBNGt(farmerNewBalance, farmerOldBalance);
 
-      apr = (farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/1800))*365;
-      apy = ((farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/1800))+1)**365;
+      apr = (farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/1565))*365;
+      apy = ((farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/1565))+1)**365;
 
       console.log("earned!");
       console.log("APR:", apr*100, "%");
